c5fc85a933a6ec60e1554e13dacb1aa448c519a4
liuyakun <953707522@qq.com>
2017-06-20 10:17:17 +0800

    修改

--- src/main/java/com/sdd/SharedConfig.java
+++ src/main/java/com/sdd/SharedConfig.java
@@ -1,11 +1,14 @@
 package com.sdd;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @ComponentScan
@@ -18,5 +21,13 @@ public class SharedConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         return template;
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+//        factory.setMaxFileSize(1024L * 1024L);
+        factory.setMaxFileSize(50*1024*1024);
+        return factory.createMultipartConfig();
     }
 }
--- src/main/resources/static/view/project/classicProjectPostCtrl.js
+++ src/main/resources/static/view/project/classicProjectPostCtrl.js
@@ -3,6 +3,17 @@
  */
 define(['../../script/sdd','jquery'],function(module,$){
     module.controller('classicProjectPostCtrl',function($resource,$scope,$rootScope,$timeout,$location){
-        console.log("经典项目内容");
+
+        $(function(){
+            $('#rightslider ul').bxSlider({pager:false,auto: true});
+        });
+
+        //监控屏幕高度
+        $("#projectcontent").height(document.body.offsetHeight-120);
+        window.addEventListener("resize", function () {
+            // 得到屏幕尺寸 (内部/外部宽度，内部/外部高度)
+            $("#projectcontent").height(document.body.offsetHeight-120);
+        }, false);
+
     });
 });
